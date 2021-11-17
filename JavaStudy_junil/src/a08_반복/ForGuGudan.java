9657d21ad0dd9804142d2dc72c00c24981b1722b
junil <skjil1218@gmail.com>
2021-11-17 21:51:13 +0900

    반복문으로 구구단 만들기

---
+++ JavaStudy_junil/src/a08_반복/ForGuGudan.java
@@ -0,0 +1,13 @@
+package a08_นÝบน;
+
+public class ForGuGudan {
+
+	public static void main(String[] args) {
+		int dan = 2;
+		for(int i = 1; i < 10; i++) {
+			System.out.println(dan + " X " + i + " = " + dan*i);
+		}
+
+	}
+
+}
