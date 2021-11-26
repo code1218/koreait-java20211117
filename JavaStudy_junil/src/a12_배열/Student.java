294bfcb9fb557e01ea8f8a7cc28cc3897fa57477
junil <skjil1218@gmail.com>
2021-11-26 21:48:32 +0900

    클래스, 배열

---
+++ JavaStudy_junil/src/a12_배열/Student.java
@@ -0,0 +1,28 @@
+package a12_¹è¿­;
+
+public class Student {
+	private String name;
+	private int year;
+	
+	public Student(String name, int year) {
+		super();
+		this.name = name;
+		this.year = year;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+}
